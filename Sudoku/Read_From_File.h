b85069e41a4da6e141e54e500b70d03c6a52b722
Sara-M8 <48355855+Sara-M8@users.noreply.github.com>
2019-11-23 20:46:33 +0200

    file read
    read from file
    can read board with bars but cant read spaces

---
+++ Sudoku/Read_From_File.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <fstream>
+class Read_From_File
+{
+public:
+	int sizeOfBoard;
+	int**arrayBoard;
+	Read_From_File();
+	~Read_From_File();
+};
+
--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#include "Read_From_File.h"
 using namespace std;
 
 int main() {
 
+	Read_From_File r;
 	return 0;
 }
